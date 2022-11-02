9696315b77b6b79d119b80b20aa3a10f7cac087e
Andrew Frankenreider <andrew.frankenreider@chrobinson.com>
2022-11-01 20:38:39 -0500

    adding some fixes for mobile

---
+++ my_webpage/static/js/sidebar.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+    $('#sub-menu').on('change', function() {
+        var url = $(this).val() // get selected value
+        if (url) {
+            // require a URL
+            window.location = url // redirect
+        }
+        return false
+    })
+})
